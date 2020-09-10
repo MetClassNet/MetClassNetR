47785062843288c32003cbf1f1f4da6e05bf9f93
Michael Witting <michael.witting@helmholtz-muenchen.de>
2020-09-10 14:28:40 +0200

    wrapper functions

---
+++ R/qfeat.R
@@ -0,0 +1,23 @@
+#'
+#'
+#' @param x `QFeature`
+#'
+qfeat_structural <- function(x, ...) {
+
+}
+
+#'
+#'
+#' @param x `QFeature`
+#'
+qfeat_statistical <- function(x, ...) {
+
+}
+
+#'
+#'
+#' @param x `QFeature`
+#'
+qfeat_homol <- function(x, ...) {
+
+}
