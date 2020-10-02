e3589f62dd1c4194e442e1bba48d90ce3b155091
Michael Witting <michael.witting@helmholtz-muenchen.de>
2020-10-02 10:06:58 +0200

    added creation of ID and checks on rtime

--- R/read.R
+++ R/read.R
@@ -10,14 +10,70 @@ readMaf <- function(x, ecol = NA, ...) {
   names(data)[names(data) == "retention_time"] <- "rtime"
 
   # check and convert rtime
-  # TODO
+  if(!any(data$rtime > 60)) {
+
+    data$rtime <- data$rtime * 60
+
+  }
+
+  # add ID if not present
+  if(!"id" %in% names(data)) {
+
+    data <- cbind(id = .createId(data), data)
+
+  }
 
   if(length(ecol == 1) && is.na(ecol)) {
-    ecol <- 22:ncol(data)
+    ecol <- 23:ncol(data)
   }
 
   # create QFeatures
   readQFeatures(data, ecol = ecol, ...)
 
+}
+
+#'
+#'
+#' place holder for mzTab
+readmzTab <- function(x) {
+
+}
+
+.createId <- function(x) {
+
+  if(!all(c("mz", "rtime") %in% colnames(x))) {
+
+    stop("missing mz and rtime information")
+
+  }
+
+  if("ccs" %in% colnames(x)) {
+
+    idlength <- nchar(nrow(x))
+
+    ids <- paste0("FT",
+                  sprintf(eval(paste0("%0", idlength, "d")), row(x)),
+                  "M",
+                  x$mz,
+                  "T",
+                  as.integer(x$rtime), # add formating on rtime
+                  "C",
+                  x$ccs)
+
+  } else {
+
+    idlength <- nchar(nrow(x))
+
+    ids <- paste0("FT",
+                  sprintf(eval(paste0("%0", idlength, "d")), row(x)),
+                  "M",
+                  x$mz,
+                  "T",
+                  x$rtime)
+
+
+  }
+
+  return(ids)
 
 }
