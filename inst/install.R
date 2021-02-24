d9d3ba2481ca3aafd5631c040775f7cf5b987fc2
Steffen Neumann <sneumann@ipb-halle.de>
2021-02-24 22:59:07 +0100

    optimised dependency installation

--- inst/install.R
+++ inst/install.R
@@ -1,8 +1,30 @@
 install.packages("devtools")
 library("devtools")
-install.packages("BiocManager")
 
-BiocManager::install("MsBackendMgf")
+##
+## CRAN
+##
+install.packages(c("nontarget", "Hmisc",
+	"dplyr", "igraph", "networkD3", "tibble", "tidyverse", "visNetwork", 
+	"rmarkdown", "knitr"))
 
+##
+## BioC
+##
+BiocManager::install(c("HDF5Array", "BiocStyle",
+	"MsCoreUtils", "Spectra", "QFeatures", #"MsBackendMgf",
+	"MSnbase", 
+	"MetNet"))
 
+##
+## Special stuff
+##
+
+install_github("MetClassNet/MetNet", ref = "devel_Liesa")
+install_github("rformassspectrometry/MsBackendMgf")
+install_github("sneumann/MsBackendMsp")
+
+## Should be in BioC ?!
+#install_github("rformassspectrometry/QFeatures")
+#install_github("rformassspectrometry/MsBackendMgf")
 
