7b72760df9a4d8fc4658d29303f380caf7eb01e9
Steffen Neumann <sneumann@ipb-halle.de>
2021-07-06 21:30:20 +0200

    Fix dependencies, Docker, bump version

--- inst/install.R
+++ inst/install.R
@@ -5,8 +5,8 @@ library("devtools")
 ## CRAN
 ##
 install.packages(c("nontarget", "Hmisc",
-	"dplyr", "igraph", "networkD3", "tibble", "tidyverse", "visNetwork", 
-	"rmarkdown", "knitr"))
+	"dplyr", "igraph", "networkD3", "tibble", "tidyverse", "visNetwork",
+	"rmarkdown", "knitr", "ggraph", "rcdk", "VennDiagram", "ontologyIndex"))
 
 ##
 ## BioC
@@ -18,12 +18,13 @@ BiocManager::install(c("HDF5Array", "BiocStyle",
 ##
 ## Special stuff
 ##
-
+install_github("aberHRML/classyfireR")
 install_github("tnaake/MetNet")
 install_github("rformassspectrometry/MsBackendMgf")
 install_github("sneumann/MsBackendMsp")
+install_github("CDK-R/rinchi")
+
 
 ## Should be in BioC ?!
 #install_github("rformassspectrometry/QFeatures")
 #install_github("rformassspectrometry/MsBackendMgf")
-
